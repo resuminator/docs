2a00d2badc43c357793edfac792b2fffef67e9c3
louremipsum <myself.vinayak2@gmail.com>
2022-10-26 19:13:09 +0530

    feat: added create-page in sidebar

--- sidebars.js
+++ sidebars.js
@@ -28,6 +28,18 @@ module.exports = {
     },
     {
       type: "category",
+      label: "User Guide",
+      collapsed: false,
+      items: [
+        {
+          type: "doc",
+          label: "Create",
+          id: "user-guide/create-page",
+        },
+      ],
+    },
+    {
+      type: "category",
       label: "Developer Guide",
       collapsed: false,
       items: [
